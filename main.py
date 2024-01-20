799e76da062502991fda20b95d231c157c363736
Andrey Bokarev <gagamen2004@gmail.com>
2024-01-20 04:30:04 +0300

    main file to start program

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello')
+from config_data.config import INTERVAL, FILE_PATH
+from synchronization.check import check
+import logs.logs
+
+
+if __name__ == '__main__':
+    if INTERVAL < 0:
+        exit('Ошибка! Интервал должен быть больше 0.')
+    logs.logs.context_logger.info(f'Программа синхронизации файлов начинает работу с директорией {FILE_PATH}')
+    while True:
+        check(INTERVAL)
