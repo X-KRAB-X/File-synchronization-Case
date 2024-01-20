155f74318af4075b7ffc94e2ee350f137e9442b1
Andrey Bokarev <gagamen2004@gmail.com>
2024-01-20 04:03:54 +0300

    return status code, delete permanently

--- api/delete.py
+++ api/delete.py
@@ -4,7 +4,8 @@ from config_data.config import TOKEN, DISK_PATH
 
 def delete(file_name):
     response = requests.delete(f'https://cloud-api.yandex.net/v1/disk/resources?path={DISK_PATH}/{file_name}&'
-                               f'permanently=false',
+                               f'permanently=true',
                                headers={"Accept": "application/json",
                                         'Authorization': TOKEN}
                                )
+    return response.status_code
