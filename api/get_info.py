36806731ad5843b4976402bd4a1666f7bd84d01c
Andrey Bokarev <gagamen2004@gmail.com>
2024-01-20 04:10:20 +0300

    added error checking

--- api/get_info.py
+++ api/get_info.py
@@ -6,7 +6,13 @@ def get_info() -> dict:
     response = requests.get(url=f'https://cloud-api.yandex.net/v1/disk/resources?path={DISK_PATH}',
                             headers={"Accept": "application/json",
                                      'Authorization': TOKEN}
-                            ).json()
+                            )
+    if response.status_code == 404:
+        exit('Ошибка! Указаная папка не найдена на Яндекс Диске.')
+    elif response.status_code == 401:
+        exit('Ошибка! Неверно указан токен.')
+
+    response = response.json()
     files_info = {}
 
     for file in response['_embedded']['items']:
