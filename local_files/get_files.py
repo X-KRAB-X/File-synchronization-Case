78035eefbd1b4909edef0493906ca11897aac398
Andrey Bokarev <gagamen2004@gmail.com>
2024-01-20 04:12:39 +0300

    added error checking

--- local_files/get_files.py
+++ local_files/get_files.py
@@ -3,4 +3,8 @@ from config_data.config import FILE_PATH
 
 
 def get_files_list():
-    return os.listdir(FILE_PATH)
+    try:
+        return os.listdir(FILE_PATH)
+    except FileNotFoundError:
+        exit('Ошибка! Неправильно указан путь к отслеживаемой папке.')
+
