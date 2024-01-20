00cceadc4b79b923d54e6b40999162e1af033e84
Andrey Bokarev <gagamen2004@gmail.com>
2024-01-20 05:55:17 +0300

    added error checking and docs

--- api/get_info.py
+++ api/get_info.py
@@ -1,8 +1,17 @@
+"""Файл для получения файлов и их хеш-сумм из облачного хранилища."""
+
 import requests
 from config_data.config import TOKEN, DISK_PATH
 
 
-def get_info() -> dict:
+def get_info() -> dict | int:
+    """
+    Функция, отправляющая запрос на получение списка файлов.
+    При этом, обрабатывает ошибки при указании токена и пути к папке на Яндекс диске.
+    Также, не получив должного ответа от сервера, возвращает код 404.
+
+    :return: Словарь, содержащий список файлов в виде ключей и их хеш-суммы в виде значений.
+    """
     response = requests.get(url=f'https://cloud-api.yandex.net/v1/disk/resources?path={DISK_PATH}',
                             headers={"Accept": "application/json",
                                      'Authorization': TOKEN}
@@ -13,9 +22,11 @@ def get_info() -> dict:
         exit('Ошибка! Неверно указан токен.')
 
     response = response.json()
-    files_info = {}
 
-    for file in response['_embedded']['items']:
-        files_info[file['name']] = file['sha256']
-
-    return files_info
+    if response.get('_embedded'):
+        files_info = {}
+        for file in response['_embedded']['items']:
+            files_info[file['name']] = file['sha256']
+        return files_info
+    else:
+        return 404
--- synchronization/check.py
+++ synchronization/check.py
@@ -1,22 +1,38 @@
-from api.get_info import get_info # файлы
-from local_files.get_sha import get_sha_dict
-from synchronization.missing_files import missing_files # 3 вида проверки
-from synchronization.new_files import new_files
-from synchronization.same_files import same_files
+"""Главый файл для сравнения файлов из обоих хранилищ."""
+
 from time import sleep
 from requests.exceptions import ConnectionError
+from api.get_info import get_info
+from local_files.get_sha import get_sha_dict
 from logs.logs import context_logger
+from synchronization.missing_files import missing_files
+from synchronization.new_files import new_files
+from synchronization.same_files import same_files
 
 
-def check(interval):
+def check(interval: int) -> None:
+    """
+    Функция проверки.
+    Получает 2 словаря с файлами и их sha256 хеш-суммами: Локальные и облачные соответственно.
+    Если не удалось получить файлы с Яндекс диска, прекращает свою работу до следующей "итерации".
+    Далее следует 3 проверки: Изменения в файлах(файлы с одним названием), новые файлы в локальном хранилище,
+    лишние файлы в облачном.
+    В случае отсутствия интернета ошибка обрабатывается и пишутся логи.
+
+    :param interval: Интервал времени для пауз между проверками.
+    """
     try:
         local_files = get_sha_dict()
         drive_files = get_info()
+        if drive_files == 404:
+            raise ConnectionRefusedError
 
         same_files(drive_files, local_files)
         new_files(drive_files, local_files)
         missing_files(drive_files, local_files)
     except ConnectionError:
-        context_logger.error('Не удалось загрузить данные с сервера. Отсутствует подключение к интернету.')
-
-    sleep(interval)
+        context_logger.error('Отсутствует подключение к интернету.')
+    except ConnectionRefusedError:
+        context_logger.error('Не удалось загрузить данные с сервера.')
+    finally:
+        sleep(interval)
